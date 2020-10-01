e22dba0640e275cb22fdb83f1f4de980ca084317
Vitaliy <vetal.morenko@mail.ru>
2020-10-01 13:06:43 +0300

    feat: add solution for count-cats and dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  return ((arguments !== 'undefined' &&  Array.isArray(arguments[0])) ? arguments[0].filter(e => typeof e === 'string').map(e => e.trim()[0].toUpperCase()).sort((a, b) => a.charCodeAt() - b.charCodeAt()).join('') : false)
 };
