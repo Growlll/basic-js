4f48bb436a5adcd3ed9c6b037c6b22d8e2ac4863
Vitaliy <vetal.morenko@mail.ru>
2020-11-01 17:23:05 +0300

    feat: add decision for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (!Array.isArray(arguments['0'])) throw new Error()
+
+  const arr = arguments['0']
+  let res = []
+  for (let i = 0; i < arr.length; i++) {
+      switch(arr[i]) {
+        case '--discard-next':
+          i++
+          break
+        case '--discard-prev':
+          if (i >= 1 && arr[i - 2] !== '--discard-next') res = res.slice(0, -1)
+          break
+        case '--double-next':
+          if (arr[i + 1] !== undefined) res.push(arr[i + 1])
+          break
+        case '--double-prev':
+          if (i >= 1 && arr[i - 2] !== '--discard-next') res.push(arr[i - 1])
+          break
+        default:
+          res.push(arr[i])
+      }
+  }
+  return res
 };
