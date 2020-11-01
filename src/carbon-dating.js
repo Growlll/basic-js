eff967b0b730b4b57f898f91b3aea24a3d9701c5
Vitaliy <vetal.morenko@mail.ru>
2020-11-01 23:26:39 +0300

    feat: add decision for tasks carbon dating, simple chain, vinigre cipher

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,5 +4,8 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-
+  const num = arguments['0']
+  if (typeof num !== 'string' || num === '' || !parseFloat(num)) return false
+  const res = Math.ceil(Math.log(MODERN_ACTIVITY / num) / (0.693 / HALF_LIFE_PERIOD))
+  return res > 0 ? res : false
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,63 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = 'true') {
+    this.mode = mode
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error()
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+    for (let i = key.length; i < message.length; i++) {
+      key += key
+    }
+    let res = ''
+    let pos = 0
+
+    for (let i = 0; i < message.length; i++) {
+      let charCode = message.charCodeAt(i)
+      if (charCode >= 65 && charCode <= 91) {
+        charCode -= 65
+        let charCodeEncrypted = key.charCodeAt(pos) - 65
+        let resCharCode = ((charCode + charCodeEncrypted) % 26) + 65
+        res += String.fromCharCode(resCharCode)
+        pos++
+      } else {
+        res += message[i]
+      }
+    }
+
+    if (!this.mode) return res.split('').reverse().join('')
+    return res
+  }
+
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error()
+    encryptedMessage = encryptedMessage.toUpperCase()
+    key = key.toUpperCase()
+    for (let i = key.length; i < encryptedMessage.length; i++) {
+      key += key
+    }
+    let res = ''
+    let position = 0
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      let charCode = encryptedMessage.charCodeAt(i)
+      if (charCode >= 65 && charCode <= 91) {
+        charCode -= 65
+        let charCodeEncrypted = key.charCodeAt(position) - 65
+        let resCharCode = ((charCode + 26 -  charCodeEncrypted) % 26) + 65
+        res += String.fromCharCode(resCharCode)
+        position++
+      } else {
+        res += encryptedMessage[i]
+      }
+    }
+
+    if (!this.mode) return res.split('').reverse().join('')
+    return res
   }
 }
 
